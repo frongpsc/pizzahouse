539860d76d8b0fcc7358b2a710599297b3eec30d
bossnattapon <boss46392@hotmail.com>
2019-04-30 02:23:14 +0700

    xD

--- Profile.php
+++ Profile.php
@@ -1,12 +1,14 @@
 
 <!DOCTYPE html>
 <?php
-            $servername = "localhost";
+            $servername = null;
             $username = "root";
             $password = "";
             $dbname = "pizzahouse";
+            $port=null;
+            $socket="/cloudsql/pizzahousenew:asia-southeast1:pizzahouse";
 
-        $conn = new mysqli($servername, $username,$password, $dbname);
+        $conn = new mysqli($servername, $username,$password, $dbname,$port,$socket);
 
         if($conn->connect_error){
                 die("Connection failed: " . $conn->connect_error);
